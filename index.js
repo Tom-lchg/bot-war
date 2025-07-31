8399c354abd51c1b18b328c038c8c868aff55aea
Tom-lchg <tommusique974@gmail.com>
2025-07-31 22:12:34 +0200

    FEAT: Ajout d'une route pour changer en temps réel move et action

--- index.js
+++ index.js
@@ -3,15 +3,30 @@ import express from 'express'
 
 const app = express()
 const port = 3000
+
 app.use(cors('*'))
+app.use(express.json())
+
+// Move et action de base dans le jeu
+let savedParameters = { move: 'UP', action: 'COLLECT' }
+
+app.post('/send-parameter', (req, res) => {
+    const { move, action } = req.body
+
+    if (!move || !action) {
+        return res.status(400).json({ error: 'Le paramètre "move" et "action" sont requis' })
+    }
+
+    savedParameters = { move, action }
+
+    res.json({ success: true, move, action })
+})
 
 app.get('/action', (_, res) => {
-    res.send({
-        move: 'DOWN',
-        action: 'COLLECT'
-    })
+    const { move, action } = savedParameters
+    res.send({ move, action })
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`http://localhost:${port}`)
 })
