ff980a9fab77b9118fdbfbc0b4d9d5b3dc64c055
Tom-lchg <tommusique974@gmail.com>
2025-07-31 22:23:19 +0200

    FEAT: Random enabled

--- index.js
+++ index.js
@@ -8,22 +8,29 @@ app.use(cors('*'))
 app.use(express.json())
 
 // Move et action de base dans le jeu
-let savedParameters = { move: 'UP', action: 'COLLECT' }
+let savedParameters = { move: 'UP', action: 'COLLECT', randomEnabled: false }
 
 app.post('/send-parameter', (req, res) => {
-    const { move, action } = req.body
+    const { move, action, randomEnabled } = req.body
 
     if (!move || !action) {
         return res.status(400).json({ error: 'Le paramètre "move" et "action" sont requis' })
     }
 
-    savedParameters = { move, action }
+    savedParameters = { move, action, randomEnabled }
 
     res.json({ success: true, move, action })
 })
 
 app.get('/action', (_, res) => {
-    const { move, action } = savedParameters
+    const { move, action, randomEnabled } = savedParameters
+
+    // Juste au cas où j'ai la flemme de faire les requests manuelles 😂
+    if (randomEnabled) {
+        savedParameters.move = ['UP', 'DOWN', 'LEFT', 'RIGHT'][Math.floor(Math.random() * 4)]
+        savedParameters.action = ['COLLECT', 'ATTACK', 'BOMB'][Math.floor(Math.random() * 3)]
+    }
+
     res.send({ move, action })
 })
 
