d8db1cc699c22470261ef8d13c011d315ea4d558
Tom-lchg <tommusique974@gmail.com>
2025-07-31 22:12:50 +0200

    TEST: Ajout du test pour tester la route "send-parameter"

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -2,12 +2,28 @@ import express from 'express'
 
 describe('Routes', () => {
     it('should return a 200 status code', () => {
+
         const app = express()
+
         app.get('/action', (_, res) => {
             res.send({
                 move: 'DOWN',
                 action: 'COLLECT'
             })
+        })
+    })
+
+    it('should return a 200 status code', () => {
+        const app = express()
+        app.get('/send-parameter', (req, res) => {
+
+            const { move, action } = req.body
+
+            if (!move || !action) {
+                return res.status(400).json({ error: 'Le paramètre "move" et "action" sont requis' })
+            }
+
+            res.status(200).json({ success: true, move, action })
         })
     })
 })
